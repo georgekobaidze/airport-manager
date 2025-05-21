6887fea2613df1a8fffcc767d1d07a94e55fdda4
Giorgi Kobaidze <george.kobaidze.14@gmail.com>
2025-05-21 21:35:59 +0400

    Add xml response formatter

--- src/AirportManager.API/Program.cs
+++ src/AirportManager.API/Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(
+    options => options.ReturnHttpNotAcceptable = true)
+    .AddXmlDataContractSerializerFormatters();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddSwaggerGen();
